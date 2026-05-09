9dd2806de630774585566beb957e665d84dca6bb
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-05-09 11:03:23 +0300

    Add files via upload

---
+++ stock_avg_unit_cost/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Stock Avg Unit Cost',
+    'version': '1.0',
+    'depends': [
+        'stock',
+        'stock_account',
+    ],
+    'data': [
+        'views/stock_valuation_layer_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
