6204a155cd7b7ac9b517d2475e3349c07a14754e
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-05-31 14:15:28 +0300

    Add author and license to manifest file

--- stock_partner_location_auto/__manifest__.py
+++ stock_partner_location_auto/__manifest__.py
@@ -1,6 +1,8 @@
 {
     'name': 'Stock Partner Destination Location',
     'version': '19.0.1.0.0',
+    'author': 'Mohammed',
+    'license': 'LGPL-3',
     'depends': ['stock'],
     'data': [],
     'installable': True,
